21a5cfd72979cc5da0ada8e806fc9563112c05c8
Flavio Ramos <prog_flavio@outlook.com>
2021-04-03 16:26:36 -0300

    implementando listando todas as Categorias

--- src/main/java/com/flavioramos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/flavioramos/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.flavioramos.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.flavioramos.cursomc.domain.Categoria;
+import com.flavioramos.cursomc.dto.CategoriaDTO;
 import com.flavioramos.cursomc.services.CategoriaService;
 
 @RestController
@@ -50,5 +53,12 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delet(@PathVariable Integer id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> finAll(){
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
