defc7c72d7625afa5bf6e5fb1d6d802b025a8c73
Flavio Ramos <prog_flavio@outlook.com>
2021-04-06 00:46:51 -0300

    Put, delete e get para cliente

---
+++ src/main/java/com/flavioramos/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,60 @@
+package com.flavioramos.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.flavioramos.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigatório!")
+	@Size(min = 5, max = 120 ,message = "Tamanho deve ser entre 5 e 120 caracteres!")
+	private String nome;
+	
+	@NotEmpty(message = "Email obrigatório!")
+	@Email(message = "Email inválido!")
+	private String email;
+	
+	public ClienteDTO() {
+	}
+
+	public ClienteDTO(Cliente obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
