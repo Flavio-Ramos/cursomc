3c1e4592ef4b02c8fa42bda5ead69858be397e6b
Flavio Ramos <prog_flavio@outlook.com>
2021-04-05 23:19:25 -0300

    validacao sintatica com o bean validation

--- src/main/java/com/flavioramos/cursomc/services/CategoriaService.java
+++ src/main/java/com/flavioramos/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.flavioramos.cursomc.domain.Categoria;
+import com.flavioramos.cursomc.dto.CategoriaDTO;
 import com.flavioramos.cursomc.repositories.CategoriaRepository;
 import com.flavioramos.cursomc.service.exceptions.DataIntegrityException;
 import com.flavioramos.cursomc.service.exceptions.ObjectNotFoundException;
@@ -59,5 +60,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page,linesPerPage,Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDto(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(),objDto.getNome());
 	}
 }
