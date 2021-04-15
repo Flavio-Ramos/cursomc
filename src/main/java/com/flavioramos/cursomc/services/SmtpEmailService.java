296f93754ed65161e32163d9a94480960af49233
Flavio Ramos <prog_flavio@outlook.com>
2021-04-15 15:47:26 -0300

    Configuracao inicial do Spring Security

--- src/main/java/com/flavioramos/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/flavioramos/cursomc/services/SmtpEmailService.java
@@ -1,9 +1,12 @@
 package com.flavioramos.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService {
 
@@ -12,11 +15,21 @@ public class SmtpEmailService extends AbstractEmailService {
 	
 	private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(MockEmailService.class);
 	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Enviando email....");
 		mailSender.send(msg);
 		LOG.info("Email enviado!");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando email....");
+		javaMailSender.send(msg);
+		LOG.info("Email enviado");
 	}
 
 }
